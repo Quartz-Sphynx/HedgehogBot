d0b79fbe468a2721d5c58b434c028c6481fb82bb
Lauren Brender <108022822+LKBrender@users.noreply.github.com>
2023-05-21 21:22:16 +0100

    main setup

---
+++ main.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace HedgehogBot
+{
+    public static class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+        }
+    }
+
+}
